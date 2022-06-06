c36e52919a7c7666dd4df93782ac921440d6b1d6
Cing <camiel.oerlemans@gmail.com>
2022-06-06 11:24:54 +0200

    list of skills

--- article_terms.py
+++ article_terms.py
@@ -1,36 +1,27 @@
-import matplotlib.pyplot as plt
-import matplotlib
-import numpy as np
-import os
-import pandas as pd
-import math
-import seaborn as sns
-
-
-
 ### Different lists per personality source
 list_personality_traits_1 = ['analytical', 'logical', 'tecgbucak', 'strong communication',
-                           'creativity skllls', 'mathematically agile', 'business savy']
+                             'creativity skllls', 'mathematically agile', 'business savy']
 list_personality_traits_2 = ['rational', 'artisan', 'guardian', 'idealist']
-list_personality_traits_3 = ['risk-averse', 'skeptical', 'pragmatic', 'matter-of-fact', ] #I think useless junk
-list_personality_traits_4 = ['Curiosity', 'Creativity', 'Critical Thinking', 'Intuition'] #is for engeneering in general
+list_personality_traits_3 = ['risk-averse', 'skeptical', 'pragmatic', 'matter-of-fact', ]  # I think useless junk
+list_personality_traits_4 = ['Curiosity', 'Creativity', 'Critical Thinking',
+                             'Intuition']  # is for engeneering in general
 
 ### List for traits employees seek
 list_seeked_traits = ['software skills', 'operational skills']
-list_seeked_traits_soft = ['management', 'critical thinking', 'mentoring', 'risk managment',
-                           'communication', 'time managment', 'cooperation',
+list_seeked_traits_soft = ['management', 'critical thinking', 'mentoring', 'risk management',
+                           'communication', 'time management', 'cooperation',
                            'presentation skills', 'team player']
 
 ### Lists for different skills per skills article source
-list_soft_skills_1 = ['communication','curiosity', 'business acumen', 'storytelling',
+list_soft_skills_1 = ['communication', 'curiosity', 'business acumen', 'storytelling',
                       'adaptability', 'critical thinking', 'product understanding',
                       'team player']
-list_soft_skills_2 = ['verbal and written communicaiton skils', 'work ethic',
+list_soft_skills_2 = ['verbal and written communication skills', 'work ethic',
                       'adaptability', 'critical thinking', 'busines acumen',
                       'collaboration']
-list_hard_skills_1 = ['statistics', 'multivariable calculus', 'linear algerbra',
-                      'programming', 'coding', 'predicitive modeling', 'machine learning',
+list_hard_skills_1 = ['statistics', 'multivariable calculus', 'linear algebra',
+                      'programming', 'coding', 'predictive modeling', 'machine learning',
                       'deep learning', 'data wrangling', 'data preparation',
                       'model deployment', 'model production', 'data visualization']
 list_soft_skills_3 = ['business knowledge', 'problem-solving', 'curiosity', 'critical thinking'
-                      'communication', 'collaboration']
+                                                                            'communication', 'collaboration']
