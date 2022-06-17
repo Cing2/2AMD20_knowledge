7eef700eb60145920fb09a933c12cd9b61e6acc5
Cing <camiel.oerlemans@gmail.com>
2022-06-17 12:42:39 +0200

    update

--- dashboard/data.py
+++ dashboard/data.py
@@ -7,6 +7,7 @@ df_all_job = pd.read_csv('output/all_jobdescriptions.csv')
 
 
 def get_word_count_jobs(word_to_search: str) -> int:
+    word_to_search = word_to_search.lower()
     df_all_job['count_word'] = df_all_job['job_description'].apply(lambda x: x.count(word_to_search))
     sum_word = df_all_job['count_word'].sum()
 
